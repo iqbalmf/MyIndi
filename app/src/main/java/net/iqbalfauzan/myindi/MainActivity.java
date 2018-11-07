354be5296669f17294ab3268ee11f7b78c719801
Iqbal Mf <iqbalmf68@gmail.com>
2018-11-07 07:40:31 +0700

    first push

---
+++ app/src/main/java/net/iqbalfauzan/myindi/MainActivity.java
@@ -0,0 +1,13 @@
+package net.iqbalfauzan.myindi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
